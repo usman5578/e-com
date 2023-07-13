1100ef74e7dc6b85e384d7771df190e83ca5b9ff
usmanazam5578 <engr.mua@gmail.com>
2023-07-13 14:01:44 +0500

    ""

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,12 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors:{
-        primaryWhite:"#ffece3",
-        cat1:"#d6d6d8",
-        cat2:"#efe1c7",
-        cat3:"#212121",
-        cat4:"#D7D7D9"
+      colors: {
+        primaryWhite: "#FFECE3",
+        cat1: "#D6D6D8",
+        cat2:"#212121",
+        cat3:"#EFE1C7",
+        cat4:"#D7D7D9",
+        opacityDownColor: "#ffffffba"
       }
     },
   },
