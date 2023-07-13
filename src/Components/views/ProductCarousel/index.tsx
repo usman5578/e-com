20fc47ea18c4c5bac4a7e653c8860a492f8bfb85
usmanazam5578 <engr.mua@gmail.com>
2023-07-13 14:15:10 +0500

    "Testing product data"

--- src/Components/views/ProductCarousel/index.tsx
+++ src/Components/views/ProductCarousel/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const ProductCarousel = () => {
+const ProductCarousel = ({ProductData}:any) => {
   return (
-    <div>ProductCarousel</div>
+    <div>{ProductData[0].productName}</div>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,23 +5,21 @@ import ProductType from '@/Components/views/ProductType'
 
 
 async function fetchAllProductsData(){
-  // let res = await fetch(`${BASE_PATH_FORAPI}/api/products`)
+  let res = await fetch(`${BASE_PATH_FORAPI}/api/products`)
 
-//   if(!res.ok){
-//     throw new Error("Failed to fetch")
-//   }
-//   return res.json();
-return {response:"Hi hello"} 
+  if(!res.ok){
+    throw new Error("Failed to fetch")
+  }
+  return res.json();
 }
 
 export default async function Home() {
   let {response} = await fetchAllProductsData();
- 
   return (
     <>
     <Hero/>
     <ProductType/>
-    <ProductCarousel/>
+    <ProductCarousel ProductData={response}/>
     </>
   )
 }
