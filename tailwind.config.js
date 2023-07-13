3f4d06d00892b7b7a00b9b0e26b2103c96da66a5
usmanazam5578 <engr.mua@gmail.com>
2023-07-13 11:47:59 +0500

    "Changes in Tailwind"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  page:{
-
-    content: [
-      './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-      './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-      './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
-    theme: {
-      extend: {
-        colors:{
-          primaryWhite:"#ffece3",
-          cat1:"#d6d6d8",
-          cat2:"#efe1c7",
-          cat3:"#212121",
-          cat4:"#D7D7D9"
-        }
-        }
-      },
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primaryWhite:"#ffece3",
+        cat1:"#d6d6d8",
+        cat2:"#efe1c7",
+        cat3:"#212121",
+        cat4:"#D7D7D9"
+      }
     },
+  },
   plugins: [],
 }
