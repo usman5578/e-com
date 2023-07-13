9f786528b6a4277df876a930ae4a20e42b6abd91
usmanazam5578 <engr.mua@gmail.com>
2023-07-14 00:57:59 +0500

    Updated tailwind and css config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,8 @@ module.exports = {
         primaryWhite: "#FFECE3",
         cat1: "#D6D6D8",
         cat2:"#212121",
-        cat3:"#EFE1C7",
-        cat4:"#D7D7D9",
+        cat3:"#D7D7D9",
+        cat4:"#EFE1C7",
         opacityDownColor: "#ffffffba"
       }
     },
