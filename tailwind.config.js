1649d699ad60e153cb8cda18d9264805bd75e8ab
usmanazam5578 <engr.mua@gmail.com>
2023-07-13 11:43:23 +0500

    "Canges in Tailwind"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      colors:{
-        primaryWhite:"#ffece3",
-        cat1:"#d6d6d8",
-        cat2:"#efe1c7",
-        cat3:"#212121",
-        cat4:"#D7D7D9"
-      }
+  page:{
+
+    content: [
+      './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+      extend: {
+        colors:{
+          primaryWhite:"#ffece3",
+          cat1:"#d6d6d8",
+          cat2:"#efe1c7",
+          cat3:"#212121",
+          cat4:"#D7D7D9"
+        }
+        }
+      },
     },
-  },
   plugins: [],
 }
