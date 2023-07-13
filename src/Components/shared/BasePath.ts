3a75dd907774a6c90663e067a139d92c088d672b
usmanazam5578 <engr.mua@gmail.com>
2023-07-13 12:32:45 +0500

    "Base Path URL"

--- src/Components/shared/BasePath.ts
+++ src/Components/shared/BasePath.ts
@@ -1,4 +1,4 @@
 const BASE_PATH_FORAPI = process.env.NODE_ENV === "development"?
-"http://localhost:3000":"e-com-orcin.vercel.app"
+"http://localhost:3000":"https://e-com-orcin.vercel.app"
 
 export default BASE_PATH_FORAPI;
