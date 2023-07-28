a722bdec394b4757c184216ea886d70f2f303028
usmanazam5578 <engr.mua@gmail.com>
2023-07-28 11:26:08 +0500

    Updated Cart

--- src/Components/views/Navbar/SubComponent/CartState.tsx
+++ src/Components/views/Navbar/SubComponent/CartState.tsx
@@ -1,14 +1,11 @@
-import { cartContext } from "@/global/context";
-import { useContext } from "react";
 import { BsCart2 } from "react-icons/bs";
 
 const CartState = () => {
-    let {state} = useContext(cartContext);
-    console.log(state.cart)
+  let data:any = localStorage.getItem("cart") as string;
   return (
     <div className="flex-shrink-0 w-11 h-11 bg-gray-200 rounded-full flex items-center justify-center relative">
       <div className=" absolute top-1 right-2 rounded-full w-4 h-4 flex items-center justify-center bg-red-400 text-xs font-light">
-        4
+        {JSON.parse(data).length}
       </div>
       <BsCart2 size={24} />
     </div>
